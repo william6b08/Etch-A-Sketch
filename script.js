2c47f55ea78a6e1008f6b8114cb97f1efd58e692
william6b08 <william6b08@yahoo.com.hk>
2022-07-21 17:18:58 +0800

    Added a set drawingboard size function

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ title.innerText='Welcome to Etch-A-Sketch!'
 title.setAttribute('style', 'text-align: center; background-color: yellow; color: orange')
 
 
+/* Create a button to set drawing board size */
+const size_btn = document.createElement('button');
+size_btn.innerText = 'Reset size';
 
 /* Create a clean button to wipe the board clean */
 const clean_btn = document.createElement('button');
@@ -48,9 +51,17 @@ current_Color_para.innerText = `Your brush is ${brush_color}`;
 
 /* Create a drawing area */
 const main_div = document.createElement('div');
+main_div.style.maxWidth = '960px';
+main_div.style.maxHeight = '960px';
+main_div.style.display = 'flex';
+main_div.style.flexDirection = 'column';
+
+
+/* Append all child elements to the webpage */
 
 body.appendChild(title);
 body.appendChild(main_div);
+body.appendChild(size_btn);
 body.appendChild(clean_btn);
 body.appendChild(current_Color_para);
 body.appendChild(current_Color);
@@ -61,31 +72,46 @@ for (const component of collection)
   component.style.margin = '10px auto';
 }
 
-/* Create a 16 x 16 divs for the drawing area */
-const row_div_attribute_value = 'justify-content: center; box-sizing: border-box';
-const col_div_attribute_value = 'box-sizing: border-box; background-color: grey; border: 1px solid black; width: 30px; height: 30px';
-
-for (let i = 0; i < 16; i++)
+/* Create a function for user to resize the drawing board */
+function createDrawingBoard()
 {
-  const row_div = document.createElement('div');
-  row_div.setAttribute('style', row_div_attribute_value);
-  row_div.style.display = 'flex';
-  main_div.appendChild(row_div);
-
-  for (let j = 0; j < 16; j++)
+  if (main_div.innerHTML)
   {
-    const col_div = document.createElement('div');
-    col_div.setAttribute('style', col_div_attribute_value);
-    col_div.classList.add('color-box');
-    row_div.appendChild(col_div);
+    main_div.innerHTML = '';
+  }
+  let number_of_squares_per_side = window.prompt("Please enter the number of squares per side you want in your drawing board.");
+  while (number_of_squares_per_side > 100)
+  {
+    window.alert("Number should smaller than 100.");
+    number_of_squares_per_side = window.prompt("Please enter the number of squares per side you want in your drawing board.")
+  }
+  /* Create a drawing area */
+  const row_div_attribute_value = `justify-content: center; box-sizing: border-box; width: 960px; height: ${960/number_of_squares_per_side}px`;
+  const col_div_attribute_value = `box-sizing: border-box; background-color: grey; border: 1px solid black; width: ${960/number_of_squares_per_side}px; height: ${960/number_of_squares_per_side}px`;
+
+  for (let i = 0; i < number_of_squares_per_side; i++)
+  {
+    const row_div = document.createElement('div');
+    row_div.setAttribute('style', row_div_attribute_value);
+    row_div.style.display = 'flex';
+    row_div.classList.add('color-box-row');
+    main_div.appendChild(row_div);
+
+    for (let j = 0; j < number_of_squares_per_side; j++)
+    {
+      const col_div = document.createElement('div');
+      col_div.setAttribute('style', col_div_attribute_value);
+      col_div.classList.add('color-box');
+      row_div.appendChild(col_div);
+    }
   }
 
-
-
+}
 
 
 function resetBoard()
 {
+  let all_Color_Boxes = document.querySelectorAll('.color-box');
   all_Color_Boxes.forEach(color_Box => {color_Box.style.backgroundColor = 'grey'})
 }
 
@@ -94,15 +120,18 @@ function pickColor(color_div)
   brush_color = color_div.style.backgroundColor;
 }
 
+function updateColorBoxLogic()
+{
+  let all_Color_Boxes = document.querySelectorAll('.color-box');
+  all_Color_Boxes.forEach((color_Box) => color_Box.addEventListener(
+    'mouseover', ()=> color_Box.style.backgroundColor = brush_color));
+}
 
-const all_Color_Boxes = document.querySelectorAll('.color-box');
+createDrawingBoard();
+updateColorBoxLogic();
 
-all_Color_Boxes.forEach((color_Box) => color_Box.addEventListener(
-  'mouseover', ()=> color_Box.style.backgroundColor = brush_color));
+
 
 clean_btn.addEventListener('click', resetBoard);
-
-
-  
-
-}
+size_btn.addEventListener('click', createDrawingBoard);
+size_btn.addEventListener('click', updateColorBoxLogic);
